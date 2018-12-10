a3631c932f70597733595d068912cb9c892e9ff4
IceCereal <ns_raghav@hotmail.com>
2018-12-10 16:33:19 +0530

    Add _read

--- readSubredditData.py
+++ readSubredditData.py
@@ -9,4 +9,28 @@ from ast import literal_eval
 class Reader:
     def __init__(self, fileName : str):
         self.fileName = fileName
-        
+
+    def _read(self):
+        #Private Function to read file and return the actual list
+
+        with open("SubredditData/"+self.fileName) as Fobj:
+            buffer = Fobj.read()    #File Data
+
+            globalList = [] #Final List
+
+            while (True):
+                start = buffer.find('$')
+                end = buffer[start+1:].find('$')
+
+                if (end == -1):
+                    unit = buffer[start+1:]
+                    readyUnit =  literal_eval(unit)
+                    globalList.append(readyUnit)
+                    break
+
+                unit = buffer[start+1 : end+1]
+                readyUnit = literal_eval(unit)
+                globalList.append(readyUnit)
+                buffer = buffer[end+1:]
+
+        return globalList
