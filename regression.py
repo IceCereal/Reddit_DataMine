a7624a4f715ffe5e4dd5692d31c04518d4867056
IceCereal <ns_raghav@hotmail.com>
2018-12-10 13:22:42 +0530

    Create Regression.py

---
+++ regression.py
@@ -0,0 +1,10 @@
+"""Regression:
+1. Linear
+2. Polynomial
+"""
+
+"""
+Y-axis Types:
+1. High Score: List[0] (i.e. Highest Popularity Index during the time)
+2. Average Score: i = 0:n do { Sigma(List[i]) / n }
+"""
