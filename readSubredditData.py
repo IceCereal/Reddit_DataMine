2f15a2122e6e466ac6c11b8d51ba7e98e821dfc5
IceCereal <ns_raghav@hotmail.com>
2018-12-10 13:30:40 +0530

    create class and __init__ for Reader

--- readSubredditData.py
+++ readSubredditData.py
@@ -4,3 +4,9 @@ Module to read and return a readable data unit for each file in SubredditData/
 Example Data:
 $['2018-11-14 21:35:18.257332', [8, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 """
+from ast import literal_eval
+
+class Reader:
+    def __init__(self, fileName : str):
+        self.fileName = fileName
+        
