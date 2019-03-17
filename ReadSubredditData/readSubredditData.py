da9d3611fe84ff7aa41dab44ae4873cc30574897
IceCereal <ns_raghav@hotmail.com>
2019-03-18 00:05:44 +0530

    Add direct call method, change reference to subreddit data

--- ReadSubredditData/readSubredditData.py
+++ ReadSubredditData/readSubredditData.py
@@ -9,129 +9,146 @@ from datetime import datetime
 from numpy import array as np_a
 
 class Reader:
-    def __init__(self, fileName : str):
-        self.fileName = fileName
+	def __init__(self, fileName : str):
+		self.fileName = fileName
 
-    def _read(self):
-        #Private Function to read file and return the actual list
+	def _read(self):
+		#Private Function to read file and return the actual list
 
-        with open("SubredditData/"+self.fileName) as Fobj:
-            buffer = Fobj.read()    #File Data
+		with open("../SubredditData/"+self.fileName) as Fobj:
+			buffer = Fobj.read()    #File Data
 
-            globalList = [] #Final List
+			globalList = [] #Final List
 
-            while (True):
-                start = buffer.find('$')
-                end = buffer[start+1:].find('$')
+			while (True):
+				start = buffer.find('$')
+				end = buffer[start+1:].find('$')
 
-                if (end == -1):
-                    unit = buffer[start+1:]
-                    readyUnit =  literal_eval(unit)
-                    globalList.append(readyUnit)
-                    break
+				if (end == -1):
+					unit = buffer[start+1:]
+					readyUnit =  literal_eval(unit)
+					globalList.append(readyUnit)
+					break
 
-                unit = buffer[start+1 : end+1]
-                readyUnit = literal_eval(unit)
-                globalList.append(readyUnit)
-                buffer = buffer[end+1:]
+				unit = buffer[start+1 : end+1]
+				readyUnit = literal_eval(unit)
+				globalList.append(readyUnit)
+				buffer = buffer[end+1:]
 
-        return globalList
+		return globalList
 
-    def _HighScore(self, data):
-        return max(data)
+	def _HighScore(self, data):
+		return max(data)
 
-    def _Mean(self, data):
-        return (sum(data)/len(data))
+	def _Mean(self, data):
+		return (sum(data)/len(data))
 
-    def _Sigma(self, data):
-        return sum(data)
+	def _Sigma(self, data):
+		return sum(data)
 
-    def getData(self, type = None, numpyEnabled = None):
-        """
-        type:
-            'high'  :   returns the highest score in the list (DEFAULT)
-            'mean'  :   returns the mean of the values in the list
-            'sigma' :   returns the sum of all the values of the list
+	def getData(self, type = None, numpyEnabled = None):
+		"""
+		type:
+			'high'  :   returns the highest score in the list (DEFAULT)
+			'mean'  :   returns the mean of the values in the list
+			'sigma' :   returns the sum of all the values of the list
 
-        numpyEnabled:
-            'No'    :   returns normal tuple (DEFAULT)
-            'Yes'   :   returns a numpy array compatible with scikit-learn
+		numpyEnabled:
+			'No'    :   returns normal tuple (DEFAULT)
+			'Yes'   :   returns a numpy array compatible with scikit-learn
 
-        Module returns a tuple:
-            ([time_1, time_2, ..., time_n], [num_1, num_2, ..., num_n])
-                time_i : float
-                    [0.00, 0.25, 0.50, ..., 23.75]
-                num_i : int
-                    [int, int, int, ..., int]
-        """
+		Module returns a tuple:
+			([time_1, time_2, ..., time_n], [num_1, num_2, ..., num_n])
+				time_i : float
+					[0.00, 0.25, 0.50, ..., 23.75]
+				num_i : int
+					[int, int, int, ..., int]
+		"""
 
-        if (type == None):
-            type = 'high'
-        
-        if numpyEnabled == None:
-            numpyEnabled = 'No'
-        
-        if (numpyEnabled not in ['No', 'Yes']):
-            return -1
+		if (type == None):
+			type = 'high'
+		
+		if numpyEnabled == None:
+			numpyEnabled = 'No'
+		
+		if (numpyEnabled not in ['No', 'Yes']):
+			return -1
 
-        if (type not in ['high', 'mean', 'sigma']):
-            return -1
+		if (type not in ['high', 'mean', 'sigma']):
+			return -1
 
-        X = []
-        y = []
+		X = []
+		y = []
 
-        def typeSelect(data):
-            #Internal Function
-            if type == 'high':
-                return self._HighScore(data)
-            if type == 'mean':
-                return self._Mean(data)
-            if type == 'sigma':
-                return self._Sigma(data)
+		def typeSelect(data):
+			#Internal Function
+			if type == 'high':
+				return self._HighScore(data)
+			if type == 'mean':
+				return self._Mean(data)
+			if type == 'sigma':
+				return self._Sigma(data)
 
-        rawData = self._read()
+		rawData = self._read()
 
-        count = 0.00
+		count = 0.00
 
-        X.append(count)
-        y.append(typeSelect(rawData[0][1]))
-        count += 1
+		X.append(count)
+		y.append(typeSelect(rawData[0][1]))
+		count += 1
 
-        for i in range(1, len(rawData)):
-            timeLower = datetime.strptime(rawData[i-1][0], '%Y-%m-%d %H:%M:%S.%f')
-            timeHigher = datetime.strptime(rawData[i][0], '%Y-%m-%d %H:%M:%S.%f')
+		for i in range(1, len(rawData)):
+			timeLower = datetime.strptime(rawData[i-1][0], '%Y-%m-%d %H:%M:%S.%f')
+			timeHigher = datetime.strptime(rawData[i][0], '%Y-%m-%d %H:%M:%S.%f')
 
-            timeDiffMins = int( (timeHigher - timeLower).total_seconds() / 60 )
+			timeDiffMins = int( (timeHigher - timeLower).total_seconds() / 60 )
 
-            if (timeDiffMins >= 16):
-                missingDataNumber = (timeDiffMins / 15) + 1
+			if (timeDiffMins >= 16):
+				missingDataNumber = (timeDiffMins / 15) + 1
 
-                dataLower = rawData[i-1][1]
-                dataHigher = rawData[i][1]
+				dataLower = rawData[i-1][1]
+				dataHigher = rawData[i][1]
 
-                dataMissing = typeSelect(dataLower) + typeSelect(dataHigher)
-                dataMissing = dataMissing/2
+				dataMissing = typeSelect(dataLower) + typeSelect(dataHigher)
+				dataMissing = dataMissing/2
 
-                for i in range(1, timeDiffMins + 1):
-                    X.append(count * 0.25)
-                    y.append(dataMissing)
-                    count += 1
+				for i in range(1, timeDiffMins + 1):
+					X.append(count * 0.25)
+					y.append(dataMissing)
+					count += 1
 
-            else:
-                X.append(count * 0.25)
-                count += 1
+			else:
+				X.append(count * 0.25)
+				count += 1
 
-                y.append(typeSelect(rawData[i][1]))
+				y.append(typeSelect(rawData[i][1]))
 
-        if numpyEnabled == 'Yes':
-            X = np_a(X).reshape(-1, 1)
-            y = np_a(y).reshape(-1, 1)
+		if numpyEnabled == 'Yes':
+			X = np_a(X).reshape(-1, 1)
+			y = np_a(y).reshape(-1, 1)
 
-        return (X, y)
+		return (X, y)
 
 if __name__ == "__main__":
-    print ("Direct Call: readSubredditData")
+	print ("Direct Call: readSubredditData")
+
+	import sys
+	"""
+		Command Line Args:
+			1. File Name (i.e. Subreddit)
+			2. Type
+			3. Numpy Enabled
+	"""	
+	print ("Subreddit:\t", sys.argv[1])
+	print ("Type:\t", sys.argv[2])
+	print ("Numpy Enabled:\t", sys.argv[3])
+
+	print ("\nStart readSubredditData")
+	Subreddit = Reader(sys.argv[1])
+	(X, y) = Subreddit.getData(type = sys.argv[2], numpyEnabled = sys.argv[3])
+
+	print (X, "\n\n", y)
 
 else:
-    print ("Setup: readSubredditData")
-    print ("Modules: Reader")
+	print ("Setup: readSubredditData")
+	print ("Modules: Reader")
